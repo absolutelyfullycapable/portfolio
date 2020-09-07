6760369c4431aa09a37d81cfde245833b59d2905
JiHye LEE <fearnonemyself@gmail.com>
2020-09-08 01:00:09 +0900

    Add files via upload
    add javascript file

---
+++ js/about.js
@@ -0,0 +1,49 @@
+$(".hoverA").hover(function() {
+    $(".hoverA").css("cursor", "pointer");
+    $(".hoverA").css("transform", "translateY(-10px)");
+    $(".hoverA").css("transition", "0.4s");
+    $(".hoverA1").css("visibility", "visible");
+}, function() {
+    $(".hoverA").css("transform", "translateY(0px)");
+    $(".hoverA1").css("visibility", "hidden");
+});
+
+$(".hoverB").hover(function() {
+    $(".hoverB").css("cursor", "pointer");
+    $(".hoverB").css("transform", "translateY(-10px)");
+    $(".hoverB").css("transition", "0.4s");
+    $(".hoverB1").css("visibility", "visible");
+}, function() {
+    $(".hoverB").css("transform", "translateY(0px)");
+    $(".hoverB1").css("visibility", "hidden");
+});
+
+$(".hoverC").hover(function() {
+    $(".hoverC").css("transform", "translateY(-10px)");
+    $(".hoverC").css("transition", "0.4s");
+    $(".hoverC1").css("visibility", "visible");
+}, function() {
+    $(".hoverC").css("transform", "translateY(0px)");
+    $(".hoverC1").css("visibility", "hidden");
+});
+
+(function (document) {
+    const markers = [...document.querySelectorAll('.slide')];
+    
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+                entry.target.style.animationPlayState = 'running';
+                observer.unobserve(entry.target);
+            }
+});
+}, {
+    threshold: 0.8
+});
+
+markers.forEach(mark => {
+    observer.observe(mark);
+});
+})(document);
+
+$(".slide").css("color", "#fff");
