ca28e197cfa7d25bae00cc20f51eed944f30e2bc
JiHye LEE <fearnonemyself@gmail.com>
2020-09-09 17:48:49 +0900

    Add files via upload
    modify errors

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-var wrapperMenu = document.querySelector('.wrapper-menu');
-
-wrapperMenu.addEventListener('click', function(){
-  wrapperMenu.classList.toggle('open');
-  $("nav").css("visibility", "visible");
-  $(".wrapper-menu").css("z-index", "9999");
-  $(".line-menu").css("background-color", "#0027c9");
-})
+$(".toggleMenu").on("click", function() {
+    $("nav").css("visibility", "visible");
+});
