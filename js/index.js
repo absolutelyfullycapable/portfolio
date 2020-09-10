e30c9b7d203a97467306436af788bd6903e04451
JiHye LEE <fearnonemyself@gmail.com>
2020-09-10 23:51:55 +0900

    Add files via upload
    modify contents

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// 모바일 메뉴 버튼 클릭 애니메이션
+// 모바일, 태블릿 햄버거 메뉴 버튼 클릭 애니메이션
 $('.menu-wrapper').on('click', function() {
         $('.hamburger-menu').toggleClass('animate');
         $('.menu').toggleClass('visible');
