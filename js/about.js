8726aea678a027b22572a3b2299467b7a13eda70
JiHye LEE <fearnonemyself@gmail.com>
2020-09-10 09:20:39 +0900

    Add files via upload
    modify js files

--- js/about.js
+++ js/about.js
@@ -1,3 +1,29 @@
+// 모바일 버튼 클릭 애니메이션
+$(".toggleMenu").on("click", function() {
+    $(".container > nav").css("visibility", "visible");
+});
+
+// 스크롤 애니메이션
+(function (document) {
+    const markers = [...document.querySelectorAll('.slide')];
+    
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+                entry.target.style.animationPlayState = 'running';
+                observer.unobserve(entry.target);
+            }
+});
+}, {
+    threshold: 0.8
+});
+
+markers.forEach(mark => {
+    observer.observe(mark);
+});
+})(document);
+
+// 호버 애니메이션
 $(".hoverA").hover(function() {
     $(".hoverA").css("cursor", "pointer");
     $(".hoverA").css("transform", "translateY(-10px)");
@@ -26,24 +52,3 @@ $(".hoverC").hover(function() {
     $(".hoverC").css("transform", "translateY(0px)");
     $(".hoverC1").css("visibility", "hidden");
 });
-
-(function (document) {
-    const markers = [...document.querySelectorAll('.slide')];
-    
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach((entry) => {
-        if (entry.intersectionRatio > 0) {
-                entry.target.style.animationPlayState = 'running';
-                observer.unobserve(entry.target);
-            }
-});
-}, {
-    threshold: 0.8
-});
-
-markers.forEach(mark => {
-    observer.observe(mark);
-});
-})(document);
-
-$(".slide").css("color", "#fff");
