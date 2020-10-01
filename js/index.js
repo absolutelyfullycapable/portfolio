6951a8fce605c6eb19232498c2416ce01a09dac3
Ji Hye <fearnonemyself@gmail.com>
2020-10-01 22:05:18 +0900

    modify index JS/jQuery

--- js/index.js
+++ js/index.js
@@ -4,23 +4,3 @@ $('.menu-wrapper').on('click', function() {
         $('.menu').toggleClass('visible');
         $('.menu-wrapper').toggleClass('visible');
 });
-
-// var app = document.getElementById('app');
-
-var typewriter = new Typewriter('#app1', {
-        loop: true
-});
-
-typewriter.typeString('ab·so·<br>lute·ly<br>fully<br>cap·<br>able') // 글자 입력
-.pauseFor(4000) // 딜레이 값
-.deleteAll() // 글자 지움
-.start();
-
-var typewriterPC = new Typewriter('#app2', {
-        loop: true
-});
-
-typewriterPC.typeString('absolutely<br>fully<br>capable') // 글자 입력
-.pauseFor(4000) // 딜레이 값
-.deleteAll() // 글자 지움
-.start();
