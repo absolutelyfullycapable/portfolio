e64105e621c6beb8bd23d2510c9fb6f29bf16a23
Ji Hye <fearnonemyself@gmail.com>
2020-10-02 18:58:38 +0900

    add works JS/jQuery

---
+++ js/works.js
@@ -0,0 +1,37 @@
+$(function() {
+    // a href="#" 기본 성격 제거
+    $('a[href="#"]').on('click', function (e) {
+        e.preventDefault();
+    });
+
+    // 모바일, 태블릿 햄버거 메뉴 버튼 클릭 애니메이션
+    $('.menu-wrapper').on('click', function() {
+        $(this).toggleClass('visible');
+        $('.hamburger-menu').toggleClass('animate');
+        $('.menu').toggleClass('active');
+        
+        // 모바일 메뉴 버튼 클릭 시 헤더 내용 숨기기
+        $('.container header').toggleClass('none');
+        $('.container header h1').toggleClass('none');
+    });
+
+    // 스크롤 시 가려진 제목 나타나는 애니메이션
+    (function (document) {
+        const markers = [...document.querySelectorAll('.slide')];
+        
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach((entry) => {
+            if (entry.intersectionRatio > 0) {
+                    entry.target.style.animationPlayState = 'running';
+                    observer.unobserve(entry.target);
+                }
+    });
+    }, {
+        threshold: 0.8
+    });
+
+    markers.forEach(mark => {
+        observer.observe(mark);
+    });
+    })(document);
+});
