f927ae34668d1679aeab33d04b0b75a1fc3801fe
absolutelyfullycapable <lovinsurfaces@gmail.com>
2020-12-19 20:44:12 +0900

    modify works HTML, CSS, JS/jQuery - title animation

--- js/works.js
+++ js/works.js
@@ -14,24 +14,4 @@ $(function() {
         $('.container header').toggleClass('none');
         $('.container header h1').toggleClass('none');
     });
-
-    // 스크롤 시 가려진 제목 나타나는 애니메이션
-    (function (document) {
-        const markers = [...document.querySelectorAll('.slide')];
-        
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach((entry) => {
-            if (entry.intersectionRatio > 0) {
-                    entry.target.style.animationPlayState = 'running';
-                    observer.unobserve(entry.target);
-                }
-    });
-    }, {
-        threshold: 0.8
-    });
-
-    markers.forEach(mark => {
-        observer.observe(mark);
-    });
-    })(document);
 });
